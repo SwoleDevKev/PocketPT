60b96ac2670a86cff24b1b3737ad72827acde8aa
Kevin Aristhomene <swoledevkev@gmail.com>
2023-12-31 14:35:55 -0500

    added modal from react bootstrap

--- src/trainerPages/TrainerPrograms/TrainerPrograms.js
+++ src/trainerPages/TrainerPrograms/TrainerPrograms.js
@@ -7,90 +7,103 @@ import WeeklyProgramCard from "../../components/WeeklyProgramCard/WeeklyProgramC
 import { Link } from "react-router-dom"
 import Header from "../../components/Header/Header"
 import TrainerFooter from "../../components/TrainerFooter/TrainerFooter"
+import {Modal} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 
-function TrainerPrograms (){
+
+function TrainerPrograms() {
 
     const [programs, setPrograms] = useState(null)
     const [weeklyPrograms, setWeeklyPrograms] = useState(null)
     const [customWeeklyPrograms, setCustomWeeklyPrograms] = useState([])
     const [user, setUser] = useState(null);
-	const [failedAuth, setFailedAuth] = useState(false);
+    const [failedAuth, setFailedAuth] = useState(false);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
 
-    useEffect(()=>{
-       async function getPrograms(){
-           const response = await axios.get(`${API_URL}/api/programs`)
-           setPrograms(response.data)
+    useEffect(() => {
+        async function getPrograms() {
+            const response = await axios.get(`${API_URL}/api/programs`)
+            setPrograms(response.data)
         }
         getPrograms()
-    },[])
+    }, [])
 
-    useEffect(()=>{
-        async function getWeeklyPrograms(){
+    useEffect(() => {
+        async function getWeeklyPrograms() {
             const response = await axios.get(`${API_URL}/api/programs/weekly`)
             console.log(response.data)
             setWeeklyPrograms(response.data)
-         }
-         getWeeklyPrograms()
-     },[])
+        }
+        getWeeklyPrograms()
+    }, [])
 
-     useEffect(()=>{
-        async function getCustomWeeklyPrograms(){
+    useEffect(() => {
+        async function getCustomWeeklyPrograms() {
             console.log(user);
             const response = await axios.get(`${API_URL}/api/programs/weekly/custom/${user?.id}`)
             console.log(response.data)
             setCustomWeeklyPrograms(response.data)
-         }
-         getCustomWeeklyPrograms()
-     },[user])
-
-    
-
-	useEffect(() => {
-		const token = sessionStorage.getItem('token')
-
-		if(!token) {
-			return setFailedAuth(true)
-		}
-
-		axios
-			.get(`${API_URL}/api/trainers/current`, {
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			})
-			.then((response) => {
-				console.log(response.data);
-				setUser(response.data)
-			})
-			.catch((error) => {
-				console.log(error);
-				setFailedAuth(true)
-			})
-
-        
-		
-	}, []);
+        }
+        getCustomWeeklyPrograms()
+    }, [user])
 
 
-	if (failedAuth) {
-		return (
-			<main className="dashboard">
-				<p>You must be logged in to see this page.</p>
-				<p>
-					<Link to="/trainer/login">Log in</Link>
-				</p>
-			</main>
-		);
-	}
+    const handlePostWeeklyProgram = () => {
 
-	if (user === null) {
-		return (
-			<main className="dashboard">
-				<p>Loading...</p>
-			</main>
-		);
-	}
+    }
+
+
+
+    useEffect(() => {
+        const token = sessionStorage.getItem('token')
+
+        if (!token) {
+            return setFailedAuth(true)
+        }
+
+        axios
+            .get(`${API_URL}/api/trainers/current`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then((response) => {
+                console.log(response.data);
+                setUser(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+                setFailedAuth(true)
+            })
+
+
+
+    }, []);
+
+
+    if (failedAuth) {
+        return (
+            <main className="dashboard">
+                <p>You must be logged in to see this page.</p>
+                <p>
+                    <Link to="/trainer/login">Log in</Link>
+                </p>
+            </main>
+        );
+    }
+
+    if (user === null) {
+        return (
+            <main className="dashboard">
+                <p>Loading...</p>
+            </main>
+        );
+    }
 
     return (
         <>
@@ -98,9 +111,9 @@ function TrainerPrograms (){
             <section className="program">
 
                 <h2 className="program__heading">Monthly Programs </h2>
-                {programs && programs.map((program)=>{
-                    return(
-                        <ProgramCard key={program.id} program={program}/>
+                {programs && programs.map((program) => {
+                    return (
+                        <ProgramCard key={program.id} program={program} />
                     )
                 })}
             </section>
@@ -108,32 +121,53 @@ function TrainerPrograms (){
             <section className="program">
 
                 <h2 className="program__heading" >Weekly Programs</h2>
-                {customWeeklyPrograms && customWeeklyPrograms.map((program)=>{
-                    return(
-                        <WeeklyProgramCard key={program.id} program={program}/>
+                {customWeeklyPrograms && customWeeklyPrograms.map((program) => {
+                    return (
+                        <WeeklyProgramCard key={program.id} program={program} />
                     )
                 })}
-                {weeklyPrograms && weeklyPrograms.map((program)=>{
-                    return(
-                        <WeeklyProgramCard key={program.id} program={program}/>
+                {weeklyPrograms && weeklyPrograms.map((program) => {
+                    return (
+                        <WeeklyProgramCard key={program.id} program={program} />
                     )
                 })}
             </section>
-            <button className="btn btn-primary">Create Weekly Program</button>
-            
+            <button onClick={handleShow} className="btn btn-primary">Create Weekly Program</button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Create A Program</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    I will not close if you click outside me. Do not even try to press
+                    escape key.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handlePostWeeklyProgram}>Understood</Button>
+                </Modal.Footer>
+            </Modal>
+
             <h2 className="program__heading" >Daily Workouts</h2>
 
             <div className="program-container">
-                <Link  className="program" to="/trainer/workouts">
+                <Link className="program" to="/trainer/workouts">
                     <h2 className="program__heading program__heading--link" >Default Daily Workouts</h2>
                 </Link>
-                <Link  className="program" to="/trainer/custom-workouts">
+                <Link className="program" to="/trainer/custom-workouts">
                     <h2 className="program__heading program__heading--link" >Custom Daily Workouts</h2>
                 </Link>
             </div>
-           
+
             <TrainerFooter />
-            
+
         </>
     )
 }
